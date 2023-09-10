52ba947365113de5922925a42d23a4263ecae46e
tyk <110590015@ntut.org.tw>
2023-09-10 16:54:08 +0800

    cart, lack of slice backend stuff

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Row } from "react-bootstrap"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,9 +7,9 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import goodsData from '@/pages/goods/goodsData.json';
 
 interface Input {
-    item_id: number | null,
+    item_id: number,
     quantity: number,
-    // updateTotal: any
+    updateTotal: (subtotal: number, id: number) => void;
 }
 
 interface Props extends Input {
@@ -18,21 +18,18 @@ interface Props extends Input {
     subtotal: number
 }
 
-// const CartItem = ({ item_id, quantity, updateTotal }: Input) => {
-//     let data: Props = { item_id, quantity, updateTotal, name: "", imgUrl: "", subtotal: 0 };
-
-const CartItem = ({ item_id, quantity }: Input) => {
-    let data: Props = { item_id, quantity, name: "", imgUrl: "", subtotal: 0 };
-
+const CartItem = ({ item_id, quantity, updateTotal }: Input) => {
+    let data: Props = { item_id, quantity, updateTotal, name: "", imgUrl: "", subtotal: 0 };
     const matchingGood = goodsData.find((goods) => goods.id === data.item_id);
 
     if (matchingGood) {
         data.name = matchingGood.name;
         data.imgUrl = matchingGood.imgUrl;
         data.subtotal = matchingGood.price * data.quantity;
+
     }
 
-    // updateTotal(data.subtotal);
+    updateTotal(data.subtotal, data.item_id);
 
     return (
         <div className='cart_item' style={{ margin: '2% 0 2% 0' }}>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,24 +1,36 @@
 import CartItem from '@/components/CartItem';
 import React, { useState } from 'react'
 import boughtData from '@/pages/cart/boughtData.json';
+import PButton from '@/components/PButton';
 
 const Cart = () => {
-    // const [total, setTotal] = useState(0);
-    // const updateTotal = (subtotal: number) => {
-    //     setTotal((prevTotal) => prevTotal + subtotal);
-    // }
+    const [total, setTotal] = useState<number[]>([]);
+    const [total_count, setTotalCount] = useState(0);
+
+    const updateTotal = (subtotal: number, id: number) => {
+        let isExist = false;
+        for (let i = 0; i < total.length; i += 2) {
+            if (total[i] === id) {
+                isExist = true;
+            }
+        }
+        if (!isExist) {
+            setTotal((prevTotal) => [...prevTotal, id, subtotal]);
+            setTotalCount((prevTotal) => prevTotal + subtotal);
+        }
+    }
 
     return (
         <div className='bg flex-wrapper'>
             <span className='titleWhite'>Cart</span>
             {boughtData.map((data) => {
                 return (
-                    <CartItem item_id={data.item_id} quantity={data.quantity} />
-                    // <CartItem item_id={data.item_id} quantity={data.quantity} updateTotal={updateTotal} />
+                    <CartItem item_id={data.item_id} quantity={data.quantity} updateTotal={updateTotal} />
                 )
             })}
             <hr className='white_bg' />
-            {/* <div className='center title2'>Total : {total}</div> */}
+            <div className='center title2'>Total : ${total_count}</div> <br />
+            <PButton text='Submit' url='' />
         </div>
     )
 }
